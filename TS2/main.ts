3b3ebeac26aea350ebc1283bb83c4dc6d688a1fa
Nicolas Salazar <n.salazars@uniandes.edu.co>
2024-10-22 09:41:07 -0500

    Copia del TS1

---
+++ TS2/main.ts
@@ -0,0 +1,25 @@
+import { Serie } from "./Serie.js";
+import { series } from "./data.js";
+
+const tBody = document.getElementById("tBody") as HTMLTableSectionElement;
+
+series.forEach((serie: Serie) => {
+  console.log("Llenando la tabla...");
+  const row = tBody.insertRow();
+  row.innerHTML = `
+        <td><b>${serie.id}</b></td>
+        <td><a href=${serie.webpage} target="_blank">${serie.name}</a></td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+    `;
+});
+console.log("Tabla llena");
+
+const averageSeasons =
+  series.reduce((sum, serie) => sum + serie.seasons, 0) / series.length;
+
+const averageRow = tBody.insertRow();
+averageRow.innerHTML = `
+    <td colspan="3"><b>Seasons average: ${averageSeasons}</b></td>
+    <td> &nbsp </td>
+`;
